208f3af81eb8e3cbaa0c6ba065caaa2194bd1be1
Eugene G <haiduchonak@gmail.com>
2014-08-21 19:53:51 +0300

    modify controller

--- src/springapp/web/InventoryController.java
+++ src/springapp/web/InventoryController.java
@@ -1,7 +1,9 @@
 package springapp.web;
 
-import org.springframework.web.servlet.mvc.Controller;
-import org.springframework.web.servlet.ModelAndView;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -9,21 +11,32 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
-import java.io.IOException;
-import java.util.Date;
+import springapp.service.ProductManager;
 
 public class InventoryController implements Controller {
 
-    protected final Log logger = LogFactory.getLog(getClass());
+	protected final Log logger = LogFactory.getLog(getClass());
 
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+	private ProductManager productManager;
 
-    	String now = (new Date()).toString();
-        logger.info("Returning hello view with " + now);
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 
-        return new ModelAndView("hello", "now", now);
-    }
+		String now = (new Date()).toString();
+		logger.info("Returning hello view with " + now);
+
+		Map<String, Object> myModel = new HashMap<String, Object>();
+		myModel.put("now", now);
+		myModel.put("products", productManager.getProducts());
+
+		return new ModelAndView("hello", "model", myModel);
+	}
+
+	public void setProductManager(ProductManager productManager) {
+		this.productManager = productManager;
+	}
 
 }
